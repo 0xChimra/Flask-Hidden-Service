6b011b69ef49f8ce53e37b3d12c690e407e97b05
BlessedNULL <Blessed_NullArray@protonmail.com>
2020-12-27 01:00:18 +0100

    Update

--- example.py
+++ example.py
@@ -1,1 +1,14 @@
- 
+from flask import Flask
+from hidden_service import run_hidden_service
+
+app = Flask(__name__)
+
+html_code = """
+This is an example text
+"""
+
+@app.route("/")
+def index():
+    return(html_code)
+
+run_hidden_service(control_port=20000, application=app, leave_address_alive=True)
